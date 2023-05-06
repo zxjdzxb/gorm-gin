bc40fc073a82a4353426bb0d9407ee87ef2bbdcf
zxjdzxb <1660154581@qq.com>
2023-05-06 15:15:40 +0800

    登录

---
+++ README.md
@@ -0,0 +1,1 @@
+go build && ./ES.exe
--- controller/UserController.go
+++ controller/UserController.go
@@ -4,10 +4,12 @@ import (
 	"ES/common"
 	"ES/model"
 	"ES/util"
-	"github.com/gin-gonic/gin"
-	"gorm.io/gorm"
 	"log"
 	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"golang.org/x/crypto/bcrypt"
+	"gorm.io/gorm"
 )
 
 func Register(c *gin.Context) {
@@ -47,10 +49,19 @@ func Register(c *gin.Context) {
 		return
 	}
 	//创建用户
+	hasedPassword, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{
+			"code":    500,
+			"message": "加密错误",
+		})
+		return
+	}
+
 	newUser := model.User{
 		Name:      name,
 		Telephone: telephone,
-		Password:  password,
+		Password:  string(hasedPassword),
 	}
 
 	db.Create(&newUser)
@@ -70,4 +81,56 @@ func isTelephoneExist(db *gorm.DB, telephone string) bool {
 		return true
 	}
 	return false
+}
+
+func Login(c *gin.Context) {
+	db := common.GetDB()
+	//获取参数
+	telephone := c.PostForm("telephone")
+	password := c.PostForm("password")
+	//数据验证
+	if len(telephone) != 11 {
+		c.JSON(http.StatusUnprocessableEntity, gin.H{
+			"code":    422,
+			"message": "手机号必须为11位",
+		})
+		return
+	}
+	if len(password) < 6 {
+		c.JSON(http.StatusUnprocessableEntity, gin.H{
+			"code":    422,
+			"message": "密码不能少于6位",
+		})
+		return
+	}
+
+	//判断手机号是否存在
+	var user model.User
+	db.Where("telephone = ?", telephone).First(&user)
+	if user.ID == 0 {
+		c.JSON(http.StatusUnprocessableEntity, gin.H{
+			"code":    422,
+			"message": "用户不存在",
+		})
+		return
+	}
+
+	//判断密码是否正确
+	if err := bcrypt.CompareHashAndPassword([]byte(user.Password), []byte(password)); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"code":    400,
+			"message": "密码错误",
+		})
+		return
+	}
+
+	//发放token
+	token := "11"
+
+	//返回结果
+	c.JSON(200, gin.H{
+		"code":    200,
+		"data":    gin.H{"token": token},
+		"message": "登录成功",
+	})
 }
