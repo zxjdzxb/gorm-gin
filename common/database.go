6376660d84c85eb87220779b16240ca3913e0197
zxj <1660154581@qq.com>
2023-05-07 13:29:08 +0800

    后台基础暂时完成

--- common/database.go
+++ common/database.go
@@ -3,6 +3,8 @@ package common
 import (
 	"ES/model"
 	"fmt"
+
+	"github.com/spf13/viper"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 )
@@ -10,12 +12,12 @@ import (
 var DB *gorm.DB
 
 func InitDb() *gorm.DB {
-	host := "localhost"
-	port := "3306"
-	database := "ginessential"
-	username := "root"
-	password := "zxj621"
-	charset := "utf8mb4"
+	host := viper.GetString("datasource.host")
+	port := viper.GetString("datasource.port")
+	database := viper.GetString("datasource.database")
+	username := viper.GetString("datasource.username")
+	password := viper.GetString("datasource.password")
+	charset := viper.GetString("datasource.charset")
 	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=%s&parseTime=true",
 		username, password, host, port, database, charset)
 	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
--- main.go
+++ main.go
@@ -2,10 +2,14 @@ package main
 
 import (
 	"ES/common"
+	"os"
+
 	"github.com/gin-gonic/gin"
+	"github.com/spf13/viper"
 )
 
 func main() {
+	InitConfig()
 
 	db := common.InitDb()
 	sqlDB, _ := db.DB()
@@ -13,6 +17,21 @@ func main() {
 
 	r := gin.Default()
 	r = CollectRoute(r)
+	port := viper.GetString("server.port")
+	if port != "" {
+		panic(r.Run(":" + port))
+	}
 	panic(r.Run()) // listen and serve on 0.0.0.0:8080
 
+}
+
+func InitConfig() {
+	workDir, _ := os.Getwd()
+	viper.SetConfigName("application")
+	viper.SetConfigType("yml")
+	viper.AddConfigPath(workDir + "/config")
+	err := viper.ReadInConfig()
+	if err != nil {
+		panic("")
+	}
 }
